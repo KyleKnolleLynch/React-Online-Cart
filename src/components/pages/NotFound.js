9cb461edde17d189186612b426cf070519d4c0ce
Kyle Lynch <kylelynch104@gmail.com>
2019-10-14 16:47:37 -0500

    deploy-prep

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFound = props => {
   return (
-    <Fragment>
+    <div className='text-center'>
       <div className='large text-danger'>
-        <h1>Error: 401!</h1>
+        <h1>Error: 404!</h1>
       </div>
-      <h2>This page does not exist.</h2>
+      <h2>
+        The page you searched for: "{props.location.pathname}" does not exist.
+      </h2>
       <h3>
         Return to{' '}
         <Link to='/' className='text-success'>
@@ -15,7 +17,7 @@ const NotFound = () => {
         </Link>{' '}
         <i className='fas fa-arrow-left'></i>
       </h3>
-    </Fragment>
+    </div>
   );
 };
 
